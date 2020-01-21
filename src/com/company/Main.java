2ad4a2dfca90e76c0dd393ba191134feaa7dcbe8
alisher-321 <alisher_abdurahmanov@inbox.ru>
2020-01-21 18:06:02 +0600

    first

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int number = 1;
+        switch (number) {
+            case 1:
+                System.out.println("число равно одному");
+                break;
+            case 2:
+                System.out.println("число равно двум");
+                break;
+            case 3:
+                System.out.println("число равно трем");
+                break;
+            default:
+                System.out.println("не угадали число");
+
+
+        }
+
+                    String name = "миранда";
+                    switch (name){
+                        case "бакыт":
+                            System.out.println("привет бакыт");
+                            break;
+                        case "миранда":
+                            System.out.println("привет миранда");
+                            break;
+                         default:
+                            System.out.println("привет ");
+
+
+
+        }
+
+    }
+}
